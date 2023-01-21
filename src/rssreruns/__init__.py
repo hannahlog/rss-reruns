c9a40bf29162e946360b2f9a103e327425de1e74
hannahlog <hannah.d.connolly@gmail.com>
2023-01-21 16:44:50 -0500

    Added __annotations__ import for Python<3.10

--- src/rssreruns/__init__.py
+++ src/rssreruns/__init__.py
@@ -1,4 +1,6 @@
 """Rebroadcast old RSS/Atom items to a new feed, in shuffled or chronological order."""
+# For the sake of Python versions < 3.10 (for stdlib generics and | for type unions)
+from __future__ import annotations
 
 from .feedmodifier import FeedModifier
 
